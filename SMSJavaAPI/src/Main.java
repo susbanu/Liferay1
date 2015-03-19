0d1731291dfbf4a62b716dbdfacb3b8d22aa0c6c
Sushant <sushant.b.java@gmail.com>
2015-03-19 10:13:06 +0530

    remove all unnecessary things

--- SMSJavaAPI/src/Main.java
+++ SMSJavaAPI/src/Main.java
@@ -8,11 +8,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		SMS sms = new SMS160by2();
-		sms.login("9662665339", "9925162820");
+		sms.login("123123", "123123");
 		System.out.println(sms.isAuthenticated());
 		try {
-		    sms.send("7801803559", "hi Sushnat!");
-		    //sms.send("9662665339", "hello!!");
+		    sms.send("123123", "hi Sushnat!");
+		    //sms.send("123123123", "hello!!");
 		} catch (NotAuthenticatedException ex) {
 		    ex.printStackTrace();
 		}
